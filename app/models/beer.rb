e6fa37d600c59b22042f7f6e726877e2d4272fe4
Mika M H Huttunen <mikhuttu@cs@da1-cs-b221-19.helsinki.fi>
2015-01-19 18:35:33 +0200

    viikko 2, teht 1-6

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,10 @@
 class Beer < ActiveRecord::Base
-	belongs_to :brewery
+  belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    sum = self.ratings.inject(0) { |s, rating| s + rating.score }
+    1.0 * sum / self.ratings.count
+  end
+
 end
