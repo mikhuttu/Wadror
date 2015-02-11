15a8b53deec697fdbdc120822f435f966ae4c25d
Huttunen Mika M H <mikhuttu@da1-cs-b221-12.helsinki.fi>
2015-02-11 15:16:15 +0200

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,14 +14,14 @@ s4 = Style.create name:"American Porter", description: "Inspired from the now wa
 Style.create name:"Sahti", description: "Said to be one of the only primitive beers to survive in Western Europe, Sahti is a farmhouse ale with roots in Finland. First brewed by peasants in the 1500s, mashing (steeping of grains) went down in wooden barrels, and then that mash would be scooped into a hand-carved wooden trough (a kuurna) with a bed of juniper twigs that acted as a filter. The bung at the bottom of the kuurna would be pulled to allow the sweet wort (liquid infusion from the mash) to pass through the twig filter, followed by wort recirculation and a hot water sparge (rinsing of the grains), all of which created a juniper infusion of sorts. Sahti is also referred to as being turbid, because the wort isn't boiled after lautering (separation of spent grain and liquid), leaving loads of proteins behind, thus providing tremendous body. A low-flocculating Finnish baker's yeast creates a cloudy unfiltered beer, with an abundance of sediment. Traditional Sahti is not typically hopped, so the task of balancing is left up to the juniper twigs, which impart an unusual resiny character and also act as a preservative. Some have compared Sahtis to German Hefeweizens, though we find them to be more akin to the Lambics of Belgium due to the exposure to wild yeast and bacteria, and its signature tartness."
 
 
-be1 = Beer.create name:"Iso 3"
-be2 = Beer.create name:"Karhu"
-be3 = Beer.create name:"Tuplahumala"
-be4 = Beer.create name:"X Porter"
-be5 = Beer.create name:"Hefezeizen"
-be6 = Beer.create name:"Helles"
-be7 = Beer.create name:"Kukko"
-be8 = Beer.create name:"Huvila Pale Ale"
+be1 = Beer.new name:"Iso 3"
+be2 = Beer.new name:"Karhu"
+be3 = Beer.new name:"Tuplahumala"
+be4 = Beer.new name:"X Porter"
+be5 = Beer.new name:"Hefezeizen"
+be6 = Beer.new name:"Helles"
+be7 = Beer.new name:"Kukko"
+be8 = Beer.new name:"Huvila Pale Ale"
 
 s1.beers << be1
 s1.beers << be2
