bd5565208d0a4e2eb88e0dc9a0913f9fd0c964b0
Mika M H Huttunen <mikhuttu@cs@da1-cs-b221-12.helsinki.fi>
2015-02-11 15:00:45 +0200

    viikko 5

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211102205) do
+ActiveRecord::Schema.define(version: 20150211145005) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
